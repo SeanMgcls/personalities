fce79497714df0ff95acb36a0a6e7ce14f6375b9
SeanMgcls <magcalassean@gmail.com>
2025-02-11 13:26:06 +0800

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 
-
-import App from "./App";
-
-const root = createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
